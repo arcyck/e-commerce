2846bca54b095913e2bc014a16fa7c321706baa4
arcyck <94861028+arcyck@users.noreply.github.com>
2025-08-18 23:04:07 -0700

    Changed formatting and return values

--- src/main/java/com/ecommerce/repository/CartItemRepository.java
+++ src/main/java/com/ecommerce/repository/CartItemRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.ecommerce.model.CartItem;
 
 @Repository
-public interface CartItemRepository extends MongoRepository<CartItem,String>{}
+public interface CartItemRepository extends MongoRepository<CartItem, String> {
+
+}
